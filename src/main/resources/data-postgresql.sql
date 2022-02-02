a6b303209676a3806b8707e2538d4fe723bd40f3
Yevhenii Pavlenko <61028083+7hiago@users.noreply.github.com>
2022-02-02 23:56:27 +0200

    Add database schema and start data

---
+++ src/main/resources/data-postgresql.sql
@@ -0,0 +1,2 @@
+INSERT INTO managers_table (manager_id, manager_firstName, manager_lastName, manager_salary, manager_hireDate, manager_phoneNumber, manager_email, manager_login, manager_password, manager_role, manager_status)
+VALUES ('M-Admin', 'Admin', 'Admin',  10000, '2020-01-01', '1', 'admin@mail.com', 'admin', '$2a$12$ahdyR3T.5gbGXz2yaKToJeegbQ0kEIG8qRXwXqvPKM.rrf577I4jG', 'ROLE_ADMIN', 'ACTIVE');
