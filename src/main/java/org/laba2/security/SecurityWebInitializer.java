4984b9daaaf2d532155c3395f686c06be16797dd
Yevhenii Pavlenko <61028083+7hiago@users.noreply.github.com>
2022-02-02 23:49:44 +0200

    Add security

---
+++ src/main/java/org/laba2/security/SecurityWebInitializer.java
@@ -0,0 +1,6 @@
+package org.laba2.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ src/main/java/org/laba2/security/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package org.laba2.security;
+
+import org.laba2.services.ManagerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@ComponentScan(basePackages = { "org.laba2.security" })
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ManagerService managerService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/").fullyAuthenticated()
+                    .antMatchers("/managers/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().successForwardUrl("/menuPage")
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout", "POST"))
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessUrl("/login");
+    }
+
+    @Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(managerService).passwordEncoder(passwordEncoder);
+    }
+}
