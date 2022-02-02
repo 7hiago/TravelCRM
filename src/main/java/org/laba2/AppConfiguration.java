584c33a58d0fa8cae6e7d54ec5f7c9e676a791b8
Yevhenii Pavlenko <61028083+7hiago@users.noreply.github.com>
2022-02-02 23:52:43 +0200

    Add main and configuration classes

---
+++ src/main/java/org/laba2/AppConfiguration.java
@@ -0,0 +1,22 @@
+package org.laba2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@Configuration
+public class AppConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
+
+}
---
+++ src/main/java/org/laba2/TravelCrmApplication.java
@@ -0,0 +1,14 @@
+package org.laba2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "org.laba2")
+public class TravelCrmApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelCrmApplication.class, args);
+    }
+
+}
