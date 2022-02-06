93c54db4ffe0e136104f5441b9142e4e06616e3c
Yevhenii Pavlenko <61028083+7hiago@users.noreply.github.com>
2022-02-06 22:28:00 +0200

    Add custom error handling

--- src/main/java/org/laba2/exception/GlobalExceptionHandlerController.java
+++ src/main/java/org/laba2/exception/GlobalExceptionHandlerController.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 
 @ControllerAdvice
@@ -13,25 +12,10 @@ public class GlobalExceptionHandlerController {
 
     private static final Logger logger = LogManager.getLogger(GlobalExceptionHandlerController.class);
 
-    @ExceptionHandler(NullPointerException.class)
-    public ModelAndView handleCreateOrderException(NullPointerException exception) {
-        logger.error("Have null in received data");
-        return new ModelAndView("./error500", "message", exception.getMessage());
-    }
-
     @ExceptionHandler(DatabaseException.class)
     public ModelAndView handleDatabaseException(DatabaseException exception) {
         logger.error("Lost connection with database");
         return new ModelAndView("./error500", "message", exception.getMessage());
-    }
-
-    @ExceptionHandler(NoHandlerFoundException.class)
-    public ModelAndView handle(Exception exception) {
-        logger.error("requested page not exist");
-        ModelAndView mv = new ModelAndView();
-        mv.addObject("message", exception.getMessage());
-        mv.setViewName("./error404");
-        return mv;
     }
 
 }
--- src/main/webapp/WEB-INF/pages/error.jsp
+++ src/main/webapp/WEB-INF/pages/error.jsp
@@ -1,8 +1,8 @@
 <%--
   Created by IntelliJ IDEA.
   User: thiago
-  Date: 29.01.2022
-  Time: 23:29
+  Date: 06.02.2022
+  Time: 22:20
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
@@ -14,10 +14,10 @@
     </style>
 </head>
 <body>
-    <div class="error">
-        <h1 class="error-title">404</h1>
-        <div class="error-description">Page not found</div>
-        <a class="btn error-btn" href="./">Back</a>
-    </div>
+<div class="error">
+    <h1 class="error-title">Something went wrong!</h1>
+    <div class="error-description">Our Engineers are on it</div>
+    <a class="btn error-btn" href="./">Back</a>
+</div>
 </body>
 </html>
